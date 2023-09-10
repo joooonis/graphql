a8dac3f5f2be770c2c31ad838f6e82b2c2b45db0
jooooonis <xowns3213@naver.com>
2023-09-11 02:02:17 +0900

    ✨ feat : typeDefs

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server';
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type Tweet {
+    id: ID!
+    text: String!
+    userId: ID!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String!
+    tweets: [Tweet]
+  }
+
+  type Query {
+    AllTweets: [Tweet!]!
+    Tweet(id: ID!): Tweet
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
